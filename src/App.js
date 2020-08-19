c449df60e3d4a63b5fbe906fe64e8c141a9624b8
Ruchir <ruchir@pdx.edu>
2020-08-19 15:51:41 -0700

    trying using checkboxes in in a branch, in this constructor wasn't used, changing that in the constructor branch

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import ToDoList from "./components/ToDoList";
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    todolist: [
-      {
-        id: 1,
-        title: "Make breakfast",
-        completed: false,
-      },
-      {
-        id: 2,
-        title: "Empty dishwasher",
-        completed: false,
-      },
-      {
-        id: 3,
-        title: "Study",
-        completed: false,
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      todolist: [
+        {
+          id: 1,
+          title: "Make breakfast",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Empty dishwasher",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Study",
+          completed: false,
+        },
+      ],
+    };
+  }
 
   render() {
     return (
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export class item extends Component {
+  constructor(props) {
+    super(props);
+    this.complete = this.complete.bind(this);
+  }
+  style = () => {
+    if (this.props.todo.completed === false) {
+      return {
+        textDecoration: "line-through",
+      };
+    }
+  };
+
+  complete(e) {}
   render() {
     return (
-      <div style={{ backgroundColor: "green" }}>
-        <p>{this.props.todo.title}</p>
+      <div style={this.style()}>
+        <p>
+          <input type="checkbox" onChange={this.complete} />
+          {this.props.todo.title}
+        </p>
       </div>
     );
   }
